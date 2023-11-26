912eca0898d05e3f36d6a9bebab87f30f6a556b3
Mujo <mbabajic.babajic56@gmail.com>
2023-11-26 01:47:42 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,163 @@
+const keyboardContainer = document.createElement("div");
+keyboardContainer.setAttribute("id", "keyboard-container");
+keyboardContainer.style.width = window.getComputedStyle(document.body).width;
+const firstRow = document.createElement("div");
+firstRow.setAttribute("class", "keyboard-row");
+const secondRow = document.createElement("div");
+secondRow.setAttribute("class", "keyboard-row");
+const thirdRow = document.createElement("div");
+thirdRow.setAttribute("class", "keyboard-row");
+const fourthRow = document.createElement("div");
+fourthRow.setAttribute("class", "keyboard-row");
+const fifthRow = document.createElement("div");
+fifthRow.setAttribute("class", "keyboard-row");
+
+function setKeyboardRow(keysArray, row) {
+  keysArray.forEach((key) => {
+    const keyBox = document.createElement("button");
+    keyBox.setAttribute("class", "key-box");
+    keyBox.textContent = key;
+
+    if (keyBox.textContent == "Bck") {
+      keyBox.setAttribute("id", "backspace-key");
+    }
+    if (keyBox.textContent == "Caps") {
+      keyBox.setAttribute("id", "caps-key");
+    }
+    if (keyBox.textContent == "Shift") {
+      keyBox.setAttribute("id", "shift-key");
+    }
+    if (keyBox.textContent == "Space") {
+      keyBox.setAttribute("id", "space-key");
+    }
+    if (keyBox.textContent == "Enter") {
+      keyBox.setAttribute("id", "enter-key");
+    }
+    row.appendChild(keyBox);
+  });
+}
+
+function appendRowsToKeyboard() {
+  const keyboardKeys = {
+    firstRowKeys: [
+      "`",
+      "1",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "0",
+      "-",
+      "=",
+      "Bck",
+    ],
+    firstRowKeysShift: [
+      "~",
+      "!",
+      "@",
+      "#",
+      "$",
+      "%",
+      "^",
+      "*",
+      "(",
+      ")",
+      "_",
+      "+",
+    ],
+    secondRowKeys: ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "[", "]"],
+    secondRowKeysShift: [
+      "Q",
+      "W",
+      "E",
+      "R",
+      "T",
+      "Y",
+      "U",
+      "I",
+      "O",
+      "P",
+      "{",
+      "}",
+    ],
+    thirdRowKeys: [
+      "Caps",
+      "a",
+      "s",
+      "d",
+      "f",
+      "g",
+      "h",
+      "j",
+      "k",
+      "l",
+      ";",
+      `'`,
+      "\\",
+    ],
+    thirdRowKeysShift: [
+      "A",
+      "S",
+      "D",
+      "F",
+      "G",
+      "H",
+      "J",
+      "K",
+      "L",
+      ":",
+      `"`,
+      "|",
+    ],
+    fourthRowKeys: [
+      "Shift",
+      "z",
+      "x",
+      "c",
+      "v",
+      "b",
+      "n",
+      "m",
+      ",",
+      ",",
+      ".",
+      "/",
+    ],
+    fourthRowKeysShift: ["Z", "X", "C", "V", "B", "N", "M", "<", ">", "?"],
+    fifthRowKeys: ["Space"],
+  };
+
+  setKeyboardRow(keyboardKeys.firstRowKeys, firstRow);
+  setKeyboardRow(keyboardKeys.secondRowKeys, secondRow);
+  setKeyboardRow(keyboardKeys.thirdRowKeys, thirdRow);
+  setKeyboardRow(keyboardKeys.fourthRowKeys, fourthRow);
+  setKeyboardRow(keyboardKeys.fifthRowKeys, fifthRow);
+
+  keyboardContainer.appendChild(firstRow);
+  keyboardContainer.appendChild(secondRow);
+  keyboardContainer.appendChild(thirdRow);
+  keyboardContainer.appendChild(fourthRow);
+  keyboardContainer.appendChild(fifthRow);
+}
+
+appendRowsToKeyboard();
+
+let IS_CAPS_ACTIVATED = false;
+
+document.body.appendChild(keyboardContainer);
+
+document.querySelector("#caps-key").addEventListener("click", () => {
+  IS_CAPS_ACTIVATED = !IS_CAPS_ACTIVATED;
+  const keys = document.querySelectorAll(".key-box");
+
+  keys.forEach((letter) => {
+    if (letter.textContent.length == 1)
+      letter.textContent = IS_CAPS_ACTIVATED
+        ? letter.textContent.toUpperCase()
+        : letter.textContent.toLowerCase();
+  });
+});
