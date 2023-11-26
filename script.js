0a6a2ba6ff2560388406885ac18f4b480a7f8e8c
Mujo <mbabajic.babajic56@gmail.com>
2023-11-26 02:34:22 +0100

    added shift lock functionality

--- script.js
+++ script.js
@@ -12,6 +12,116 @@ fourthRow.setAttribute("class", "keyboard-row");
 const fifthRow = document.createElement("div");
 fifthRow.setAttribute("class", "keyboard-row");
 
+const keyboardKeys = {
+  firstRowKeys: [
+    "`",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "0",
+    "-",
+    "=",
+    "Bck",
+  ],
+  firstRowKeysShift: [
+    "~",
+    "!",
+    "@",
+    "#",
+    "$",
+    "%",
+    "^",
+    "&",
+    "*",
+    "(",
+    ")",
+    "_",
+    "+",
+    "Bck",
+  ],
+  secondRowKeys: ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "[", "]"],
+  secondRowKeysShift: [
+    "Q",
+    "W",
+    "E",
+    "R",
+    "T",
+    "Y",
+    "U",
+    "I",
+    "O",
+    "P",
+    "{",
+    "}",
+  ],
+  thirdRowKeys: [
+    "Caps",
+    "a",
+    "s",
+    "d",
+    "f",
+    "g",
+    "h",
+    "j",
+    "k",
+    "l",
+    ";",
+    `'`,
+    "\\",
+    "Enter",
+  ],
+  thirdRowKeysShift: [
+    "Caps",
+    "A",
+    "S",
+    "D",
+    "F",
+    "G",
+    "H",
+    "J",
+    "K",
+    "L",
+    ":",
+    `"`,
+    "|",
+    "Enter",
+  ],
+  fourthRowKeys: [
+    "Shift",
+    "z",
+    "x",
+    "c",
+    "v",
+    "b",
+    "n",
+    "m",
+    ",",
+    ",",
+    ".",
+    "/",
+  ],
+  fourthRowKeysShift: [
+    "Shift",
+    "Z",
+    "X",
+    "C",
+    "V",
+    "B",
+    "N",
+    "M",
+    "<",
+    ">",
+    "?",
+  ],
+  fifthRowKeys: ["Space"],
+};
+
 function setKeyboardRow(keysArray, row) {
   keysArray.forEach((key) => {
     const keyBox = document.createElement("button");
@@ -34,100 +144,6 @@ function setKeyboardRow(keysArray, row) {
 }
 
 function appendRowsToKeyboard() {
-  const keyboardKeys = {
-    firstRowKeys: [
-      "`",
-      "1",
-      "2",
-      "3",
-      "4",
-      "5",
-      "6",
-      "7",
-      "8",
-      "9",
-      "0",
-      "-",
-      "=",
-      "Bck",
-    ],
-    firstRowKeysShift: [
-      "~",
-      "!",
-      "@",
-      "#",
-      "$",
-      "%",
-      "^",
-      "*",
-      "(",
-      ")",
-      "_",
-      "+",
-    ],
-    secondRowKeys: ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "[", "]"],
-    secondRowKeysShift: [
-      "Q",
-      "W",
-      "E",
-      "R",
-      "T",
-      "Y",
-      "U",
-      "I",
-      "O",
-      "P",
-      "{",
-      "}",
-    ],
-    thirdRowKeys: [
-      "Caps",
-      "a",
-      "s",
-      "d",
-      "f",
-      "g",
-      "h",
-      "j",
-      "k",
-      "l",
-      ";",
-      `'`,
-      "\\",
-      "Enter",
-    ],
-    thirdRowKeysShift: [
-      "A",
-      "S",
-      "D",
-      "F",
-      "G",
-      "H",
-      "J",
-      "K",
-      "L",
-      ":",
-      `"`,
-      "|",
-    ],
-    fourthRowKeys: [
-      "Shift",
-      "z",
-      "x",
-      "c",
-      "v",
-      "b",
-      "n",
-      "m",
-      ",",
-      ",",
-      ".",
-      "/",
-    ],
-    fourthRowKeysShift: ["Z", "X", "C", "V", "B", "N", "M", "<", ">", "?"],
-    fifthRowKeys: ["Space"],
-  };
-
   setKeyboardRow(keyboardKeys.firstRowKeys, firstRow);
   setKeyboardRow(keyboardKeys.secondRowKeys, secondRow);
   setKeyboardRow(keyboardKeys.thirdRowKeys, thirdRow);
@@ -144,6 +160,7 @@ function appendRowsToKeyboard() {
 appendRowsToKeyboard();
 
 let IS_CAPS_ACTIVATED = false;
+let IS_SHIFT_ACTIVATED = false;
 
 document.body.appendChild(keyboardContainer);
 
@@ -159,4 +176,30 @@ function capsLock() {
   });
 }
 
+function shiftLock() {
+  IS_SHIFT_ACTIVATED = !IS_SHIFT_ACTIVATED;
+
+  const normalKeysContent = [
+    ...keyboardKeys.firstRowKeys,
+    ...keyboardKeys.secondRowKeys,
+    ...keyboardKeys.thirdRowKeys,
+    ...keyboardKeys.fourthRowKeys,
+  ];
+
+  const shiftedKeysContent = [
+    ...keyboardKeys.firstRowKeysShift,
+    ...keyboardKeys.secondRowKeysShift,
+    ...keyboardKeys.thirdRowKeysShift,
+    ...keyboardKeys.fourthRowKeysShift,
+  ];
+  const keys = document.querySelectorAll(".key-box");
+
+  for (let i = 0; i < shiftedKeysContent.length; i++) {
+    keys[i].textContent = IS_SHIFT_ACTIVATED
+      ? shiftedKeysContent[i]
+      : normalKeysContent[i];
+  }
+}
+
 document.querySelector("#caps-key").addEventListener("click", capsLock);
+document.querySelector("#shift-key").addEventListener("click", shiftLock);
